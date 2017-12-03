ed6a3742b20283c9bba1b24d727f84694e61cb66
Laurie Brown <laurie@indieskies.com>
2017-12-03 18:07:19 +0000

    added getCharInMap

--- Source/Renderer.py
+++ Source/Renderer.py
@@ -9,6 +9,9 @@ def setCharInMap(inMap, inPos, inChar):
     mapArray[inPos[1]] = mapArray[inPos[1]][:inPos[0]] + inChar + mapArray[inPos[1]][inPos[0] + 1:]
     return mapArray
 
+def getCharInMap(inMap, inPos):
+    return inMap[inPos[1]][inPos[0]]
+
 def mapToString(inMap):
     mapString = ''
     for i, line in enumerate(inMap):
